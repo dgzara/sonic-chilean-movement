5927782e00df78f6b20307983e078c0b072bb1ad
Diego Gomez-Zara <dgomezara@gmail.com>
2016-11-14 13:42:46 -0600

    explorando con REM

--- code/analysis/rem.R
+++ code/analysis/rem.R
@@ -18,6 +18,53 @@ as.sociomatrix.eventlist <- function (eventlist, n)
   g
 }
 
+format_data <- function(data)
+{
+  # Obtenemos los usuarios
+  users <- data.frame(sort(unique(c(unique(sort(data$FromId)), unique(sort(data$ToId))))), stringsAsFactors=FALSE)
+  users <- cbind(as.numeric(rownames(users)), users)
+  colnames(users) <- c("id", "username")
+  
+  # Identiticamos el tipo de usuario
+  users$is_leader <- (users$username %in% leaders)
+  users$is_org <- (users$username %in% orgs)
+  users$type <- 0
+  if(nrow(users[(users$is_leader == 1),])>0){users[(users$is_leader == 1),]$type <- 1}
+  if(nrow(users[(users$is_org == 1),])>0){users[(users$is_org == 1),]$type <- 2}
+  
+  # Reemplazamos los nombres por ids
+  data$FromId <- match(data$FromId,users$username)
+  data$ToId <- match(data$ToId,users$username)
+  
+  # Ordenamos la matriz por la fecha
+  data$StartTime <- as.numeric(data$StartTime)
+  data$StartTime <- data$StartTime - min(data$StartTime)
+  data <- data[order(data[,1]),]
+  rownames(data) <- NULL
+  
+  # Si vemos tiempos iguales, le agregamos una unidad. 
+  temp <- 0
+  for(i in 2:nrow(data))
+  {
+    if(data[i,]$StartTime == temp)
+    {
+      repetidos <- data[(data$StartTime == temp),]
+      for(j in 1:nrow(repetidos)-1)
+      {
+        data[i +(j-1),]$StartTime <- temp + j/10
+      }
+      rm(repetidos)
+    } else {
+      temp <- data[i,]$StartTime
+    }
+  }
+  
+  # Ponemos la fila final
+  data <- rbind(data, c(max(data$StartTime)+1,NA,NA, NA, NA))
+  
+  return(list(data, users))
+}
+
 # Obtenemos los tweets dirigidos
 q <- paste('SELECT lower(source) as source, lower(target) as target, type, hashtag, datetime  
            FROM hashtags_network',sep="")
@@ -34,121 +81,138 @@ tweets <- tweets[!(tweets$target %in% celebrities),]
 tweets <- tweets[!(tweets$target %in% media),]
 
 # Seleccionamos un hashtag de ejemplo, en la cadena de retweets
-data <- tweets[(tweets$hashtag == "4deagosto" & tweets$type == "retweet"),]
+data <- tweets[(tweets$type != "retweet"),]
 data[,c("type", "hashtag")] <- NULL
 data <- data[, c("datetime", "source", "target")]
 
-# Buscamos los usuarios que participan
-users <- sort(unique(c(unique(sort(data$source)), unique(sort(data$target)))))
-users <- as.data.frame(users)
-colnames(users) <- c("username")
+# Creamos la red
+g <- graph.data.frame(data[,c("source","target")], directed=TRUE)
+E(g)$timestamp <- data$datetime
 
-# Seleccionamos si son lideres o no
-users$type <- 0
-users[(users$username %in% leaders),]$type <- 1
-users[(users$username %in% orgs),]$type <- 2
+# Simplificamos
+g <- simplify(g, remove.multiple = FALSE, remove.loops = TRUE)
 
-# Eliminamos las interacciones persona-persona
-# data <- merge(data, users[,c("username","type")], by.x = "source", by.y = "username")
-# data <- merge(data, users[,c("username","type")], by.x = "target", by.y = "username")
-# data <- data[(data$type.x > 0 | data$type.y > 0 ),]
-# data <- data[,c("datetime", "source", "target")]
+# Seteamos los tipos
+V(g)$type <- 0
+V(g)[V(g)$name %in% leaders]$type <- 1
+V(g)[V(g)$name %in% orgs]$type <- 2
 
-# Removemos a los usuarios de las comunidades más isoladas
-set.seed(245)
-graph <- graph.data.frame(data[,2:3], directed = TRUE)
-wc <- cluster_walktrap(graph)
-V(graph)$community <- membership(wc)
-vertex.isolated <- V(graph)[community > 2]$name
-data <- data[!(data$source %in% vertex.isolated),]
-data <- data[!(data$target %in% vertex.isolated),]
+# Obtengo las mejores 10 organizaciones y 10 líderes
+top.leaders <- rev(sort(degree(g, mode="in")[V(g)$name %in%leaders]))[1:5]
+top.orgs <- rev(sort(degree(g, mode="in")[V(g)$name %in%orgs]))[1:5]
 
-# Actualizamos la lista
-users <- as.data.frame(sort(unique(c(unique(sort(data$source)), unique(sort(data$target))))))
-users <- cbind(rownames(users), users)
-colnames(users) <- c("id", "username")
-users$is_leader <- (users$username %in% leaders)
-users$is_org <- (users$username %in% orgs)
-users$type <- 0
-users[(users$is_leader == 1),]$type <- 1
-users[(users$is_org == 1),]$type <- 2
+# Generamos de nuevo la red
+g <- induced.subgraph(g, which(V(g)$type==0 | V(g)$name %in% names(top.leaders) | V(g)$name %in% names(top.orgs)))
 
-# Reemplazamos los nombres por ids
-data$source <- match(data$source,users$username)
-data$target <- match(data$target,users$username)
+# Creamos los k-cores
+coreness <- graph.coreness(g)
+g <- induced.subgraph(g, which(coreness > 5))
 
-# Ordenamos la matriz por la fecha
-data$time <- as.numeric(data$datetime)
-data$time <- data$time - min(data$time)
-data <- data[order(data[,4]),]
-data <- data[,c("time", "source", "target")]
-colnames(data) <- c("StartTime", "FromId", "ToId")
-rownames(data) <- NULL
+# Generamos los clusters
+cluster <- cluster_walktrap(g)
+cluster.affiliation <- membership(cluster)   # affiliation list
 
-# Si vemos tiempos iguales, le agregamos una unidad. 
-temp <- 0
-for(i in 2:nrow(data))
-{
-  if(data[i,]$StartTime == temp)
-  {
-    repetidos <- data[(data$StartTime == temp),]
-    for(j in 1:nrow(repetidos)-1)
-    {
-      data[i +(j-1),]$StartTime <- temp + j/10
-    }
-    rm(repetidos)
-  } else {
-    temp <- data[i,]$StartTime
-  }
-}
+# Obtengo los clústers de los líderes y orgs
+com.leaders <- cluster.affiliation[(names(cluster.affiliation) %in% leaders)]
+com.orgs <- cluster.affiliation[(names(cluster.affiliation) %in% orgs)]
 
-# Ponemos la fila final
-data <- rbind(data, c(max(data$StartTime)+1,NA,NA))
+# Genero las redes donde hay líderes y organizaciones
+g.leaders <- induced.subgraph(g, which(cluster$membership %in% com.leaders & V(g)$type != 2))
+g.orgs <- induced.subgraph(g, which(cluster$membership %in% com.orgs & V(g)$type != 1))
 
-# We build the network
-network <- as.sociomatrix.eventlist(data, nrow(users))
-gplot(network, vertex.col=4-2*users$type, vertex.cex=2, edge.lwd=network^0.75)
+# Visualizamos
+plot(g.leaders, vertex.color=V(g.leaders)$type)
+plot(g.orgs, vertex.color=V(g.orgs)$type)
+
+# Generamos la lista de edges
+data.leaders <- data.frame(E(g.leaders)$timestamp, get.edgelist(g.leaders)[,1], get.edgelist(g.leaders)[,2], stringsAsFactors=FALSE)
+data.orgs <- data.frame(E(g.orgs)$timestamp, get.edgelist(g.orgs)[,1], get.edgelist(g.orgs)[,2], stringsAsFactors=FALSE)
+colnames(data.leaders) <- colnames(data.orgs) <- c("StartTime", "FromId", "ToId") 
+
+# Formateamos la data
+data.leaders <- format_data(data.leaders)
+data.orgs <- format_data(data.orgs)
+
+################################
+####
+## LIDERES
+####
+################################
+
+# Creamos la primera red
+network.leaders <- as.sociomatrix.eventlist(data.leaders[[1]], nrow(data.leaders[[2]]))
+gplot(network.leaders, vertex.col=4-2*data.leaders[[2]]$type, vertex.cex=2, edge.lwd=network.leaders^0.75)
 
 # 1st model: intercept model, containing only a vector of 1s (ClassIntercept) 
-intercept <- rep(1, nrow(users)) 
-classfit1 <- rem.dyad(data, n=nrow(users), effects=c("CovSnd"), covar=list(CovSnd=intercept), ordinal=FALSE, hessian=TRUE)
-summary(classfit1)
-
-# Results
-(classfit1$m-1)/max(data[,1])                     #Events per minute (on average) 
-nrow(users)*(nrow(users)-1)*exp(classfit1$coef)   #Predicted events per minute
-
-# 2nd model: Adding the effects of Leadership/Orgs
-classfit2 <- rem.dyad(data,n=nrow(users),effects=c("CovSnd","CovRec"), 
-                    covar=list(CovSnd=cbind(intercept,users$is_leader,users$is_org), 
-                               CovRec=cbind(users$is_leader,users$is_org)), ordinal=FALSE, hessian=TRUE)
-summary(classfit2)
-classfit1$BIC-classfit2$BIC
-
-# 3rd model: Probando como atributo
-classfit3 <- rem.dyad(data,n=nrow(users),effects=c("CovSnd","CovRec"), 
-                      covar=list(CovSnd=cbind(intercept,users$type), 
-                                 CovRec=cbind(users$type)), ordinal=FALSE, hessian=TRUE)
-summary(classfit3)
-classfit2$BIC-classfit3$BIC         # Descartado
-
-# 4th model: Removing the type variable in propensity to send
-classfit4 <-rem.dyad(data,n=nrow(users),effects=c("CovSnd","CovRec"),
-                     covar=list(CovSnd=cbind(intercept),
-                                CovRec=cbind(users$is_leader,users$is_org)),ordinal=FALSE,hessian=TRUE)
-summary(classfit4)
-classfit3$BIC-classfit4$BIC #Reduced model is indeed preferred
-
-# 5th mdoel: Recency effects would seem to be a reasonable bet
-classfit5 <- rem.dyad(data,n=nrow(users),effects=c("CovSnd","CovRec","RRecSnd","RSndSnd"),
-                    covar=list(CovSnd=cbind(intercept),
-                               CovRec=cbind(users$is_leader,users$is_org)),ordinal=FALSE,hessian=TRUE)
-summary(classfit5)
-classfit4$BIC-classfit5$BIC 
+intercept.leaders <- rep(1, nrow(data.leaders[[2]])) 
+classfit.leaders.1 <- rem.dyad(data.leaders[[1]], n=nrow(data.leaders[[2]]), effects=c("CovSnd"), covar=list(CovSnd=intercept.leaders), ordinal=FALSE, hessian=TRUE)
+summary(classfit.leaders.1)
 
 # we incorporate the appropriate P-shift effects into our cumulative model:
-classfit6 <- rem.dyad(data,n=nrow(users),effects=c("CovSnd","CovRec", "RRecSnd","PSAB-BA","PSAB-AY","PSAB-BY"),
-                      covar=list(CovSnd=cbind(intercept),
-                                 CovRec=cbind(users$is_leader,users$is_org)),ordinal=FALSE,hessian=TRUE)
-summary(classfit6)
-classfit5$BIC-classfit6$BIC #Enhanced model is again preferred
+classfit.leaders.2 <- rem.dyad(data.leaders[[1]], n=nrow(data.leaders[[2]]), effects=c("CovSnd","CovRec", "RRecSnd","PSAB-BA","PSAB-AY","PSAB-BY"),
+                      covar=list(CovSnd=cbind(intercept.leaders, data.leaders[[2]]$is_leader),
+                                 CovRec=cbind(data.leaders[[2]]$is_leader)),ordinal=FALSE,hessian=TRUE)
+summary(classfit.leaders.2)
+classfit.leaders.1$BIC-classfit.leaders.2$BIC #Enhanced model is again preferred
+
+# we incorporate the appropriate P-shift effects into our cumulative model:
+classfit.leaders.3 <- rem.dyad(data.leaders[[1]], n=nrow(data.leaders[[2]]), effects=c("CovSnd", "CovRec", "RRecSnd", "RSndSnd", "PSAB-BA", "PSAB-AY", "PSAB-BY", "PSA0-XA", "PSAB-XA", "PSAB-XB"),
+                            covar=list(CovSnd=cbind(intercept.leaders, data.leaders[[2]]$is_leader),
+                                       CovRec=cbind(data.leaders[[2]]$is_leader)),ordinal=FALSE,hessian=TRUE)
+summary(classfit.leaders.3)
+classfit.leaders.2$BIC-classfit.leaders.3$BIC 
+
+exp(classfit.leaders.2$coef[c("CovSnd.2")])
+exp(classfit.leaders.2$coef[c("PSAB-BA")]) # Reciprocity
+exp(classfit.leaders.2$coef[c("PSAB-AY")]) # 
+exp(classfit.leaders.2$coef[c("PSAB-BY")]) # Transivity
+
+# Checking the model
+hist(classfit.leaders.2$residuals)
+mean(apply(classfit.leaders.2$predicted.match,1,all))
+mean(apply(classfit.leaders.2$predicted.match,1,any))
+
+################################
+####
+## ORGS
+####
+################################
+
+# Creamos la segunda red
+network.orgs <- as.sociomatrix.eventlist(data.orgs[[1]], nrow(data.orgs[[2]]))
+gplot(network.orgs, vertex.col=4-2*data.orgs[[2]]$type, vertex.cex=2, edge.lwd=network.orgs^0.75)
+
+# 1st model: intercept model, containing only a vector of 1s (ClassIntercept) 
+intercept.orgs <- rep(1, nrow(data.orgs[[2]])) 
+classfit.orgs.1 <- rem.dyad(data.orgs[[1]], n=nrow(data.orgs[[2]]), effects=c("CovSnd"), covar=list(CovSnd=intercept.orgs), ordinal=FALSE, hessian=TRUE)
+summary(classfit.orgs.1)
+
+# we incorporate the appropriate P-shift effects into our cumulative model:
+classfit.orgs.2 <- rem.dyad(data.orgs[[1]], n=nrow(data.orgs[[2]]), effects=c("CovSnd","CovRec","RRecSnd","RSndSnd","PSAB-BA","PSAB-AY","PSAB-BY"),
+                               covar=list(CovSnd=cbind(intercept.orgs, data.orgs[[2]]$is_org),
+                                          CovRec=cbind(data.orgs[[2]]$is_org)),ordinal=FALSE,hessian=TRUE)
+summary(classfit.orgs.2)
+classfit.orgs.1$BIC-classfit.orgs.2$BIC 
+
+# we incorporate the appropriate P-shift effects into our cumulative model:
+classfit.orgs.3 <- rem.dyad(data.orgs[[1]], n=nrow(data.orgs[[2]]), effects=c("CovSnd", "CovRec", "RRecSnd", "RSndSnd", "PSAB-BA", "PSAB-AY", "PSAB-BY", "PSA0-XA", "PSAB-XA", "PSAB-XB"),
+                            covar=list(CovSnd=cbind(intercept.orgs, data.orgs[[2]]$is_org),
+                                       CovRec=cbind(data.orgs[[2]]$is_org)),ordinal=FALSE,hessian=TRUE)
+summary(classfit.orgs.3)
+classfit.orgs.2$BIC-classfit.orgs.3$BIC 
+
+exp(classfit.orgs.2$coef[c("CovSnd.2")]) 
+exp(classfit.orgs.2$coef[c("PSAB-BA")]) # Reciprocity
+exp(classfit.orgs.2$coef[c("PSAB-AY")]) # 
+exp(classfit.orgs.2$coef[c("PSAB-BY")]) # Transivity
+
+#Organization responding to another student
+exp(sum(classfit.orgs.3$coef[c("CovSnd.1","CovSnd.2","PSAB-AY")]))
+
+#Get the surprising events, and display as a network
+surprising<-as.sociomatrix.eventlist(data.orgs[[1]][classfit.orgs.3$observed.rank>19,], nrow(data.orgs[[2]]))
+gplot(surprising, vertex.col=4-2*data.orgs[[2]]$type, vertex.cex=2)
+
+#Show how the "surprising" events fit into the broader communication structure 
+edgecol<-matrix(rgb(surprising/(network.orgs+0.01),0,0),nrow(data.orgs[[2]]),nrow(data.orgs[[2]])) #Color me surprised 
+gplot(network.orgs,edge.col=edgecol,edge.lwd=network.orgs^0.75,vertex.col=4-2*data.orgs[[2]]$type,vertex.cex=2)
